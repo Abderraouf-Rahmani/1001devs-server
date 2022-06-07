aa073b5b83e29a77d0cf6595b87df427b979eec7
ABDERRAOUF RAHMANI <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-06-07 13:54:24 +0000

    added the login and register authes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
+const authRoute = require('./routes/Auth')
 const MONGO_URL = process.env['MONGO_URL']
+
+app.use(express.json());
 
 mongoose.connect(MONGO_URL).then(console.log('connected to MDB')).catch((err)=>console.log(err))
 
-app.get('/d', (req, res)=>console.log('Ugly face'))
+app.use('/api/auth', authRoute)
 
 app.listen(3030, ()=>console.log('connected to server'))
